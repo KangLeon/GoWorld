ce5aea2ee3aee328dfb414509f6d3c38f9e20e4b
Leon Kang <kangleonjy@gmail.com>
2020-12-08 23:47:26 +0800

    new
    new

--- 2020-11-24/awesomeProject/hello.go
+++ 2020-11-24/awesomeProject/hello.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/cmplx"
 )
 
 //函数外面也可以定义变量
@@ -40,6 +42,51 @@ func variableShorter() {
 	fmt.Println(a,b,c,s)
 }
 
+//内建类型：原生支持负数类型
+func euler() {
+	fmt.Println(cmplx.Pow(math.E,1i*math.Pi)+1)
+}
+
+//强制类型转换
+func triangle() {
+	var a,b int = 3,4
+	var c int
+	//下面必须强制类型转换，负责会报错
+	c = int(math.Sqrt(float64(a*a+b*b)))
+	fmt.Println(c)
+}
+
+//常量
+func consts() {
+	const filename = "abc.txt"
+	const a, b = 3, 4
+	var c int
+	c= int(math.Sqrt(a*a+b*b))
+	fmt.Println(filename,c)
+}
+
+//枚举
+func enums()  {
+	//普通枚举类型
+	const (
+		cpp = iota
+		java
+		python
+		golang
+	)
+
+	//自增值的枚举
+	const (
+		b = 1 << (10*iota)
+		kb
+		mb
+		gb
+		tb
+		pb
+	)
+	fmt.Println(cpp,java,python,golang)
+}
+
 func main() {
 	fmt.Println("Hello world")
 	variableZeroValue()
@@ -48,4 +95,7 @@ func main() {
 	variableShorter()
 	fmt.Println(aa,ss,bb)
 	fmt.Println(aa1,ss1,bb1)
+	euler()
+	consts()
+	enums()
 }
