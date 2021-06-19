bfe902ade748ae96db5347fc8da6326571797577
jitengjiao <15048471585@163.com>
2021-06-19 16:10:47 +0800

    new
    new

--- 2021-03-18/testProject/main.go
+++ 2021-03-18/testProject/main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello,world")
+
+	var variables1 int
+	variables2 := "自动推断"
+
+	var variables3 = 100
+	var variables4,variables5,variables6,variables7 = 1,"字符串","3.13",true
+
+	fmt.Print(variables1,variables2,variables3,variables4,variables5,variables6,variables7)
 }
