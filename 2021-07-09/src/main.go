0af93cd5a91be5809ccac93386744c49be030704
jitengjiao <15048471585@163.com>
2021-07-09 15:46:00 +0800

    new
    new

--- 2021-07-09/src/main.go
+++ 2021-07-09/src/main.go
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-07-09 15:18:23
- * @LastEditTime: 2021-07-09 15:21:39
+ * @LastEditTime: 2021-07-09 15:45:09
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /2021-07-09/main.go
  */
-package _021_07_09
+package main
 
 import "fmt"
 
