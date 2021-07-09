8017a7c8ba928878ae0e11dcbb645a75279286e6
jitengjiao <15048471585@163.com>
2021-07-09 15:44:39 +0800

    new
    new

---
+++ 2021-07-09/src/main.go
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-09 15:18:23
+ * @LastEditTime: 2021-07-09 15:21:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /2021-07-09/main.go
+ */
+package _021_07_09
+
+import "fmt"
+
+func main() {
+	/* 这是我的第一个简单的程序 */
+	fmt.Println("Hello, World!")
+}
