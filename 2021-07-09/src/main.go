7d727b578b325094b974e5d96ffc9ad1c5c1c7dc
JY <15048471585@163.com>
2021-07-15 23:43:51 +0800

    new
    new

--- 2021-07-09/src/main.go
+++ 2021-07-09/src/main.go
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2021-07-09 15:18:23
- * @LastEditTime: 2021-07-10 17:00:21
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /2021-07-09/main.go
- */
 package main
 
 import "fmt"
