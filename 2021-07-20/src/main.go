22b06967c3f13b93a782ee69e7cb94c7468c79df
jitengjiao <15048471585@163.com>
2021-07-20 17:59:15 +0800

    通过HandleFunc启动一个http服务
    通过HandleFunc启动一个http服务

--- 2021-07-20/src/main.go
+++ 2021-07-20/src/main.go
@@ -1,2 +1,15 @@
-package src
+package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func ServerHttp(w http.ResponseWriter,r *http.Request)  {
+	fmt.Fprintln(w,"通过HandleFunc启动一个http服务")
+}
+
+func main() {
+	http.HandleFunc("/",ServerHttp)
+	http.ListenAndServe("8080",nil)
+}
