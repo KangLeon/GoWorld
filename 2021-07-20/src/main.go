5b951454f2cb466dd7d4b29e6887905f32c7e8fb
jitengjiao <15048471585@163.com>
2021-07-20 18:32:06 +0800

    new
    new

--- 2021-07-20/src/main.go
+++ 2021-07-20/src/main.go
@@ -9,7 +9,18 @@ func ServerHttp(w http.ResponseWriter,r *http.Request)  {
 	fmt.Fprintln(w,"通过HandleFunc启动一个http服务")
 }
 
+type DefineServerMux struct {
+
+}
+
+func (dsm *DefineServerMux) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer,"通过自定义类型启动一个http")
+}
+
 func main() {
 	http.HandleFunc("/",ServerHttp)
-	http.ListenAndServe("8080",nil)
+	defineServerMux := DefineServerMux{}
+	http.Handle("getUserInfo",&defineServerMux)
+
+	http.ListenAndServe(":8000",nil)
 }
