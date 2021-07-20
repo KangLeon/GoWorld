e384b2f9202d287b6d1ab8f12494447a3e7533f6
JY <15048471585@163.com>
2021-07-20 23:13:01 +0800

    new
    new

--- 2021-07-20/src/main.go
+++ 2021-07-20/src/main.go
@@ -3,29 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 type DefineServerMux struct {
-
 }
 
 func (dsm *DefineServerMux) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(writer,"通过自定义类型启动一个http")
+	fmt.Fprintln(writer, "通过自定义类型启动一个http")
 }
 
-func ServerHttp(w http.ResponseWriter,r *http.Request)  {
-	fmt.Fprintln(w,"通过HandleFunc启动一个http服务")
+func ServerHttp(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "通过HandleFunc启动一个http服务")
 
-	fmt.Fprintln(w,r.URL.RawQuery)
-	fmt.Fprintln(w,r.URL.Host)
-	fmt.Fprintln(w,r.URL.Path)
+	fmt.Fprintln(w, r.URL.RawQuery)
+	fmt.Fprintln(w, r.URL.Host)
+	fmt.Fprintln(w, r.URL.Path)
+	rawquery := r.URL.RawQuery
+	va, _ := url.ParseQuery(rawquery)
+
+	fmt.Fprintln(w, "获取url中key为name的值", va.Get("name"))
 }
 
 func main() {
-	http.HandleFunc("/geturl",ServerHttp)
+	http.HandleFunc("/geturl", ServerHttp)
 
 	defineServerMux := DefineServerMux{}
-	http.Handle("/getUserInfo",&defineServerMux)
+	http.Handle("/getUserInfo", &defineServerMux)
 
-	http.ListenAndServe(":8000",nil)
+	http.ListenAndServe(":8000", nil)
 }
