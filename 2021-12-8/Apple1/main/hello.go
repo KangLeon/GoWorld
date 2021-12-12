651f838ab037dc85ed0c80c0e7091561f7a9d22d
JY <15048471585@163.com>
2021-12-12 21:48:59 +0800

    new

--- 2021-12-8/Apple1/main/hello.go
+++ 2021-12-8/Apple1/main/hello.go
@@ -13,6 +13,25 @@ func main() {
 		End:      "def",
 		Duration: 3600,
 		FreeCent: 10000,
+		StartPos: &trippb.Location{
+			Latitude:  30,
+			Longitude: 120,
+		},
+		EndPos: &trippb.Location{
+			Latitude:  35,
+			Longitude: 115,
+		},
+		PathLocations: []*trippb.Location{
+			{
+				Latitude:  31,
+				Longitude: 119,
+			},
+			{
+				Latitude:  32,
+				Longitude: 118,
+			},
+		},
+		Status: trippb.TripStatus_IN_PROGRESS,
 	}
 	fmt.Println(&trip)
 
