2ddf5f9baa1be12f1d04f627cc9a588b8d0425eb
JY <15048471585@163.com>
2021-12-19 21:15:59 +0800

    new

--- 2021-12-8/Apple1/main.go
+++ 2021-12-8/Apple1/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	trippb "Apple1/proto/gen/go"
 	trip "Apple1/tripservice"
+	"context"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"net/http"
+	"runtime"
 )
 
 func main() {
+	log.SetFlags(log.Lshortfile)
+	go startGRPCGetway()
 	lis, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatalf("Failed to listn: %v", err)
@@ -19,4 +24,27 @@ func main() {
 		&trip.Service{},
 	)
 	log.Fatal(s.Serve(lis))
+}
+
+func startGRPCGetway() {
+	c := context.Background()
+	c, cancel := context.WithCancel(c)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	err := trippb.RegisterTripServiceHandlerFromEndpoint(
+		c,
+		mux,
+		":8081",
+		[]grpc.DialOption{grpc.WithInsecure()},
+	)
+
+	if err != nil {
+		log.Fatalf("Cannot start grpc gateway: %v", err)
+	}
+
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("Cannot listen and server: %v", err)
+	}
 }
