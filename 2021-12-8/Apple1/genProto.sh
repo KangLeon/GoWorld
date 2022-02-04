0f0facfff81d136a4ca0a429f30c6963a75839cc
JY <15048471585@163.com>
2022-02-04 11:14:29 +0800

    new
    new

--- 2021-12-8/Apple1/genProto.sh
+++ 2021-12-8/Apple1/genProto.sh
@@ -1,2 +1,6 @@
+PROTO_PATH=./auth/api
+GO_OUT_PATH=./auth/api/gen/v1
+mkdir -p $GO_OUT_PATH
+
 protoc -I=. --go-grpc_out=paths=source_relative:gen/go trip.proto --go_out=paths=source_relative:gen/go trip.proto
 protoc -I=. --grpc-gateway_out=paths=source_relative,grpc_api_configuration=trip.yaml:gen/go trip.proto
